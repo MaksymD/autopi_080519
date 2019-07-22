5b6f6f625e298789829a419483efb4d4c54eeed4
maksymd <maksym.dotsenko@gmail.com>
2019-07-22 07:53:34 +0200

    diff

--- src/salt/my/sleeplog.py
+++ src/salt/my/sleeplog.py
@@ -1,14 +1,28 @@
-from src.salt.base.ext._modules.power import hibernate
+# from src.salt.base.ext._modules.power import hibernate
+import mmap
+import os
 
 
-def sleepcan(textContent, keyword):
-    if lines=textContent.split("\n")
-    count=len([1 for line in lines if line.find(keyword)!=-1])
-    return count
+def errors_scan():
+    path = '/home/maksymd/Desktop/Work/Aviloo/test/'
+    filename = 'testlog'
+    for filename in os.listdir(path):
+        f = open(filename, 'r')
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        if s.find('error') != -1:
+            print('True')
+        else:
+            print('False')
 
-    file=open(r "\var\log'salt\minion", "r", encoding="utf-8-sig")
-
->>> yourTextFile="hello world\n some words here\n goodbye world"
->>> kwdCount(ourTextFile,"ERROR")
-    2:
-        hibernate() #calling hibernat00e function
+#
+# def sleepcan(textContent, keyword):
+#     if lines=textContent.split("\n")
+#     count=len([1 for line in lines if line.find(keyword)!=-1])
+#     return count
+#
+#     file=open(r "\var\log'salt\minion", "r", encoding="utf-8-sig")
+#
+# >>> yourTextFile="hello world\n some words here\n goodbye world"
+# >>> kwdCount(ourTextFile,"ERROR")
+#     2:
+#     hibernate() #calling hibernate function
